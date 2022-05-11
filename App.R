080abf98d46d7da39f28cc23f7486a4438b103b3
qktttt <qik@miamioh.edu>
2022-05-10 21:04:32 -0400

    Finished version of project

--- App.R
+++ App.R
@@ -4,7 +4,7 @@ library(shinyjs)
 library(plotly)
 library(ggmap)
 library(ggplot2)
-library(ggthemes) 
+library(ggthemes)
 library(tidyverse)
 library(shinyWidgets)
 
@@ -16,41 +16,44 @@ ui <- fluidPage(
   # Application title
   titlePanel(title = "New York Airbnb"),
   sidebarLayout(
-    
+
     # Sidebar typically used to house input controls
     sidebarPanel(
       # put your input controls here!
+      width = 4,
       conditionalPanel(
-        condition = "input.tabs == 'Mapplot' ", 
+        condition = "input.tabs == 'Visualization with Map' ",
         selectInput(inputId="roomType", label="Select a Airbnb Type",
-                    choices=c("All rooms" = "all",
+                    choices=c("All rooms" = "all type",
                               "Private Room"="Private room",
                               "Entire Home/Apartment"="Entire home/apt",
-                              "Shared Room"="Shared room")), 
+                              "Shared Room"="Shared room")),
+        plotlyOutput("histogram"),
+        span(tags$h3(textOutput("mapDescription")))
       ),
       conditionalPanel(
-        condition = "input.tabs == 'Statplot' ", 
+        condition = "input.tabs == 'Visualization by Area' ",
         selectInput(inputId="area", label="Select a Area",
-                    choices=(unique(Airbnb2$Area))), 
+                    choices=(unique(Airbnb2$Area))),
       )
     ),
     # Main panel typically used to display outputs
     # To display multiple tabs, use "Tabsets"
     # refer to https://shiny.rstudio.com/articles/layout-guide.html
-    
+
     # mainPanel(
     #   plotOutput(outputId="timeplot")
     # )
     mainPanel(
       tabsetPanel(
         id="tabs",
-        tabPanel("Mapplot", plotlyOutput("Mapplot")), 
-        tabPanel("Statplot", plotlyOutput("Statplot"), 
-                 plotlyOutput("PercentagePlot")) #, 
+        tabPanel("Visualization with Map", plotlyOutput("Mapplot")),
+        tabPanel("Visualization by Area", plotlyOutput("Statplot"),
+                 plotlyOutput("PercentagePlot")) #,
         #tabPanel("CorrePlot")
       )
-    )    
-    
+    )
+
   )
 )
 
@@ -58,14 +61,17 @@ ui <- fluidPage(
 ### Define server behavior for application here
 server <- function(input, output) {
   selectedGroupedData <- grouped_by_data
-  
+  storedSubarea <- reactiveVal("New York City")
+  storedSubareaData <- reactiveVal(Airbnb2)
+  roomType <- reactiveVal("all types")
+
   textFunction <- function(dta) {
     map(
       paste(
         ' <b>Area:</b>',
         dta$Area,
         '<br>',
-        '<b>Neighborhood:</b>', 
+        '<b>Neighborhood:</b>',
         dta$Subarea,
         '<br>',
         '<b>Average Price:</b>',
@@ -74,54 +80,115 @@ server <- function(input, output) {
       HTML
     )
   }
-  
+
   output$Mapplot <- renderPlotly({
-    if (input$roomType != "all") {
+    if (input$roomType != "all type") {
       selectedGroupedData <- grouped_by_data_withType %>% filter(room_type == input$roomType)
     }
-    
-    breaks = quantile(log(selectedGroupedData$meanPrice), c(0.05, 0.5, 0.75, 0.95))
+
+    breaks = quantile(log(selectedGroupedData$meanPrice), c(0.05, 0.5, 0.75, 0.95, 0.99))
     labels <- round(exp(breaks))
-    
-    result <- ggmap(newYorkMap) + geom_point(data=selectedGroupedData, aes(x=long, y=lat, colour=log(meanPrice), 
-                              text=textFunction(selectedGroupedData))) + 
+
+    result <- ggmap(newYorkMap) + geom_point(data=selectedGroupedData, aes(x=long, y=lat, colour=log(meanPrice),
+                              text=textFunction(selectedGroupedData)), size=2.5) +
       scale_colour_continuous(low="lightgray", high="darkred",
                             breaks = breaks,
                             labels = labels)  +
-      theme(legend.title = element_text("Average Price")) 
-      
-    ggplotly(result, height=900, width=1100, tooltip = "text")
-    
+      theme(legend.title = element_text("Average Price"), plot.title = element_text(hjust = 0.5)) +
+      ggtitle(paste("Airbnb Neighborhoods Average Price Map -", input$roomType, "of Airbnb")) + labs(x="", y="", colour = "Average Price")
+
+    ggplotly(result, height=700*1.1, width=1000*1.1, tooltip = "text")
+
   })
-  
+
   output$Statplot <- renderPlotly({
     airbnbNumber <- Airbnb2 %>% group_by(Area) %>% count()
     airbnbNumber <- airbnbNumber %>% arrange(n)
     result <- ggplot() + geom_histogram(aes(x=Area, y=n), data=airbnbNumber, stat="identity", fill="orange", binwidth = 0.5) + labs(
       x = "District Name", y = "Number of Airbnb", title = "Number of Airbnb for different Areas"
-    )
+    ) + theme(plot.title = element_text(hjust = 0.5))
     ggplotly(result, width=600)
-  }) 
-  
+  })
+
+  output$histogram <- renderPlotly({
+    data <- event_data("plotly_click")
+    selectedData <- NULL
+    if (is.null(data)) {
+      selectedSubarea = "New York City"
+      selectedData <- Airbnb2
+
+    } else {
+      print(data)
+      if (as.integer(data["curveNumber"]) == 1) {
+        selectedSubarea = "New York City"
+        selectedData <- Airbnb2
+      } else {
+        selectedX <- as.numeric(data["x"])
+        selectedY <- as.numeric(data["y"])
+
+        #print(grouped_by_data)
+        print(selectedX)
+        print(selectedY)
+        distances <- sqrt((grouped_by_data$long * 1000 - selectedX * 1000)^2 + (grouped_by_data$lat * 1000 - selectedY * 1000)^2)
+        print(length(distances))
+        print(distances)
+        selectedSubarea <- grouped_by_data$Subarea[which(distances == min(distances))]
+        print(selectedSubarea)
+        selectedData <- Airbnb2 %>% filter(Subarea == selectedSubarea)
+      }
+    }
+
+
+    if (input$roomType != "all type") {
+      selectedData <- selectedData %>% filter(room_type == input$roomType)
+      roomType(paste(input$roomType, "type"))
+    } else {
+      roomType("all types")
+    }
+
+    storedSubarea(selectedSubarea)
+    storedSubareaData(selectedData)
+    #print(selectedData)
+    result <- ggplot() + geom_histogram(data=selectedData, aes(x=price), binwidth = 10, color="orange") + xlim(0, min(800, max(selectedData$price) + 10)) +
+      ggtitle(paste("Airbnb price Histogram \n for:", selectedSubarea, "-", input$roomType)) + theme(plot.title = element_text(hjust = 0.5))
+    ggplotly(result, width=550)
+  })
+
+
+
+  # ploting the percentage plot
   output$PercentagePlot <- renderPlotly({
-    if (input$area != "") { 
+    if (input$area != "") {
       selectedDta <- Airbnb2 %>% filter(Area == input$area)
     } else {
       selectedDta <- Airbnb2
     }
-    
+
     selectedDta_Count <- selectedDta %>% group_by(room_type) %>% count() %>% mutate(room_type = as.factor(room_type))
     oevrallSum <- sum(selectedDta_Count$n)
     selectedDta_Count <- selectedDta_Count %>% mutate(percentage = n / oevrallSum)
-    
+
     perPlot <- ggplot(selectedDta_Count, aes(x = room_type, y = percentage, fill = room_type)) +
                 geom_histogram(stat="identity", fill="lightblue") + labs(
                   x = "Room Type",
                   y = "Percentage of room type",
                   title = paste("Percentage of Three Room Types for Area: ", input$area, sep="")
-                )
-    
-    ggplotly(perPlot, width=600)
+                ) + theme(plot.title = element_text(hjust = 0.5))
+
+    ggplotly(perPlot, width=500)
+  })
+
+  output$mapDescription <- renderText({
+    result <- paste("Current seleced area is: ", storedSubarea(), ".", sep="")
+    numAvai <- NROW(storedSubareaData())
+    meanPrice <- mean(storedSubareaData()$price)
+    medianPrice <- median(storedSubareaData()$price)
+    maxPrice <- max(storedSubareaData()$price)
+    minPrice <- min(storedSubareaData()$price)
+    result <- paste(result, paste(" In this area, considering Airbnb of ", roomType(),
+                                  ", there are ", as.integer(numAvai), " Airbnbs available. The average Airbnb price at here is ",sprintf("%.2f", meanPrice),
+    ". The maximum, minimum Airbnb price in this area are, respectively: ", sprintf("%.2f", maxPrice),
+    " ", sprintf("%.2f", minPrice), ". And the median price is: ", sprintf("%.2f", medianPrice), ".", sep=""))
   })
 }
 
--- DataPreparation.R
+++ DataPreparation.R
@@ -19,23 +19,23 @@ Airbnb2 <- Airbnb %>%
          "Nights" = minimum_nights,
          "Reviews" = number_of_reviews,
          "Availability" = availability_365,
-         "Review_date" = ymd(last_review)) %>% 
+         "Review_date" = ymd(last_review)) %>%
   filter(Nights > 0,
-         Availability > 0) %>% 
-  select(Apt_Name, Host, Area, Subarea, latitude, longitude, 
-         room_type, price, Nights, Reviews, Review_date, Availability) %>% 
-  arrange(desc(price)) %>% 
+         Availability > 0) %>%
+  select(Apt_Name, Host, Area, Subarea, latitude, longitude,
+         room_type, price, Nights, Reviews, Review_date, Availability) %>%
+  arrange(desc(price)) %>%
   drop_na()
- 
+
 #### Overall mean price grouped by sub_area
 grouped_by_data <- Airbnb2 %>% group_by(Area, Subarea) %>% summarise(meanPrice = mean(price),
                                                                      long=mean(longitude), lat = mean(latitude))
 
-#### Sub area mean price grouped by, also with diffrent type of airbnbs 
+#### Sub area mean price grouped by, also with diffrent type of airbnbs
 grouped_by_data_withType <- Airbnb2 %>% group_by(Area, Subarea, room_type) %>% summarise(meanPrice = mean(price),
                                       long=mean(longitude), lat = mean(latitude))
 
-register_google("AIzaSyAC7FXgey9NKHFRx2zFwYIetoD09MHuxl4")  
+register_google("###################")
 newYorkMap <- get_map("New York", zoom = 10, size = c(1000, 1000))
 
 ## save data
